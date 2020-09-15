b4349320765f2de98adcb9680db59dd71d16c8aa
xhwhust <364968567@qq.com>
2020-09-15 10:55:52 +0800

    add check & save image

--- Main/urls.py
+++ Main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.mainPage),
     re_path(r'^classify/$', views.classifyImage),
+    re_path(r'^saveImage/$', views.saveImage),
 ]
--- Main/views.py
+++ Main/views.py
@@ -4,12 +4,8 @@ import json
 import base64
 import numpy as np
 import cv2
-
-import sys
-
-# sys.path.append("")
 from ImageNet import VGGLib
-from ImageNet.VGGLib import VGGNet
+import os
 
 
 # Create your views here.
@@ -22,12 +18,33 @@ def mainPage(request):
 
 
 def classifyImage(request):
-    if request.method == "POST":
-        image = request.POST.get("image")
-        image = json.loads(image)
-        image = str(image).split(';base64,')[1]
-        image = base64.b64decode(image)
-        image = np.fromstring(image, np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        result = VGGLib.classifyImage(image)
+    image = request.POST.get("image")
+    image = json.loads(image)
+    image = str(image).split(';base64,')[1]
+    image = base64.b64decode(image)
+    image = np.fromstring(image, np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    result = VGGLib.classifyImage(image)
     return HttpResponse(json.dumps(result))
+
+
+def saveImage(request):
+    typeName = request.POST.get("typeName")
+    image = request.POST.get("image")
+    fileName = hash(image)
+    image = json.loads(image)
+    image = str(image).split(';base64,')[1]
+    image = base64.b64decode(image)
+
+    path = "media/" + request.user.username + "/" + typeName
+    print(path, os.path.exists(path))
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    filePath = os.path.join(path, str(fileName) + ".jpg")
+    file = open(filePath, "wb")
+    file.write(image)
+    file.close()
+
+    print("save => ", filePath)
+    return HttpResponse("success")
